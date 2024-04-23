0be7a49f1c09503502e9ad75ef1ef950a91c75d4
Xander <xwhitby20@gmail.com>
2024-04-23 17:06:03 -0600

    made x fill up the spaces on the board

--- tictactoe.py
+++ tictactoe.py
@@ -1,24 +1,26 @@
-# def board_display(board):
-#     return print(board)
+def board_display(board):
+    return board
 
 # game menu
-def start_game(start, name, display_name, first_move):
-    return start, name, display_name, first_move
+# def start_game(start, name, display_name, first_move):
+#     return start, name, display_name, first_move
+    
 
 
-# def decisions():
+# def decisions(first_move):
 #     pass
 
 
 
 # board display
-# board_vertical_lines_1 = ('      |  |\n   ----------\n      |  |\n   ----------\n      |  |\n')
-# board_display(board_vertical_lines_1)
+mark = 'x'
+board_vertical_lines_1 = print(f'   {mark} | {mark} | {mark}\n  -----------\n   {mark} | {mark} | {mark}\n  -----------\n   {mark} | {mark} | {mark}\n')
+board_display(board_vertical_lines_1)
 
 
 # start game variables
-welcome = print('Welcome to Tic Tac Toe')
-user_name = input('What is your username? ')
-your_name = print(f'Your name is {user_name}!')
-first_or_second = input('Would you like to go first or second? ').lower
-start_game(welcome, user_name, your_name, first_or_second)
+# welcome = print('Welcome to Tic Tac Toe')
+# user_name = input('What is your username? ')
+# your_name = print(f'Your name is {user_name}!')
+# first_or_second = input('Would you like to go first or second? ').lower
+# start_game(welcome, user_name, your_name, first_or_second)
